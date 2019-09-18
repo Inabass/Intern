664d0f15149515c59061fa00bbf432474bbe0bf3
kamata-masato <sen.masatokamata@gmail.com>
2019-09-18 12:08:57 +0900

    authコマンドで認証系のファイル作成

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,16 +1,32 @@
 <?php
-
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
+---ログイン
+　・ログイン画面 作成
+　・テストデータ(users) 挿入
+　・ユーザー認証機能 追加-確認
+
+---ホーム
+　・ホーム画面(イベント一覧) 作成
+　・テストデータ(users-events)
+　・イベント一覧 確認
+
+---イベント作成
+　・イベント作成画面 作成
+　・イベント作成機能＊(認証キー発行)＊ 追加-確認
+
+---イベント編集
+　・イベント編集画面 作成
+　・イベント編集機能 追加-確認
+
+---イベント削除確認
+　・イベント削除確認画面 作成
+　・イベント削除機能 追加-確認
+
+---イベント編集
+　・テストデータ(events-images)
+　・写真登録機能 追加-確認
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
